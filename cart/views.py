93eb41e28b7e392e123a06db76f3f624ba7e3f04
Abolfazl Babaei <abolfazl.9599@gmail.com>
2025-03-16 14:08:04 +0330

    add docs to cart app
    fix bug in products app urls

--- cart/views.py
+++ cart/views.py
@@ -2,51 +2,31 @@ from .models import Cart
 from .serializers import (CartSerializer, UpdateCartSerializer)
 from rest_framework import viewsets, status, views
 from rest_framework.response import Response
+
+
 # Create your views here.
 
 
 class CartViewSet(viewsets.ViewSet):
+    """
+    ViewSet for managing the user's cart.
+    Retrieves and returns the list of items in the user's cart.
+    """
 
     def list(self, request):
         cart = Cart.objects.filter(user=request.user)
         serializer = CartSerializer(cart, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def create(self, request):
-    #     serializer = AddItemToCartSerializer(data=request.data, context={'request': request})
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-
-# class RemoveCartItemView(views.APIView):
-#     def put(self, request):
-#         cart = Cart.objects.filter(user=request.user).first()
-#         if not cart:
-#             return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
-#         serializer = DecreaseItemFromCartSerializer(instance=cart, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#
-#
-# class DeleteCartItemView(views.APIView):
-#     def put(self, request):
-#         cart = Cart.objects.filter(user=request.user).first()
-#         if not cart:
-#             return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
-#         serializer = DeleteItemFromCartSerializer(instance=cart, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'message': 'Product Item deleted in Cart'}, status=status.HTTP_200_OK)
-
-
 class UpdateCartView(views.APIView):
+    """
+    API view for adding, updating, or removing items from the user's cart.
+    Ensures the cart exists and updates it based on the request data.
+    """
 
     def post(self, request, *args, **kwargs):
         cart, _ = Cart.objects.get_or_create(user=request.user)
-        serializer = UpdateCartSerializer(instance=cart,data=request.data)
+        serializer = UpdateCartSerializer(instance=cart, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.update(cart, serializer.validated_data)
         return Response({'message': 'cart updated'}, status=status.HTTP_200_OK)
--- products/urls.py
+++ products/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'products'
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
-    path('<slug>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('categories/', views.CategoryListView.as_view(), name='categories'),
     path('features/', views.ProductFutureListView.as_view(), name='features'),
     path('galleries/', views.ProductGalleryListView.as_view(), name='galleries'),
 
     path('create-comment/', views.ProductCommentCreateView.as_view(), name='product_add_comment'),
+
+    path('<slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 
 ]
